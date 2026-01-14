caf1d578a1eeb1f92c29fb5a6059bcb09dc6b95c
Nikhil-11-11-2007 <ng364357@gmail.com>
2026-01-14 17:41:21 +0530

    Second commit

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 const board = document.querySelector('.board')
-const blockHeight = 30
-const blockWidth = 30
+const blockHeight = 40
+const blockWidth = 40
 const cols = Math.floor(board.clientWidth / blockWidth);
-const rows = Math.floor(board.clientHeight / blockHeight)
+const rows = Math.floor(board.clientHeight / blockHeight);
 
-for(let i = 0; i< cols*rows; i++){
-    const block = document.createElement('div')
-    block.classList.add('block')
-    board.appendChild(block)
-}
+const blocks = []
+const snake = [
+    {x: 1, y: 3}
+]
 
-for(let row = 0; row < rows; row++){
-    for(let col = 0; col < cols; col++){
-        
+let direction = 'left'
+
+for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        board.appendChild(block)
+        block.innerText = `${row},${col}`
+        blocks[`${row},${col}`] = block
     }
 }
+
+function render() {
+    snake.forEach((elem) => {
+        blocks[`${elem.x},${elem.y}`].classList.add('fill')
+    })
+}
+
+setInterval(() => {
+    let head = null
+
+    if(direction === 'left'){
+        head = {x: snake[0].x, y: snake[0].y - 1}
+    }
+
+    snake.forEach((elem) => {
+        blocks[`${elem.x},${elem.y}`].classList.remove('fill')
+    })
+
+    snake.unshift(head)
+    snake.pop()
+    render()
+}, 400);
